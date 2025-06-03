a0c13dde482ee00e8883eeca877af3bbeaed9096
ricardo-martinez-hefesto-pragma <ricardo.martinez+hefesto@pragma.co>
2025-06-03 15:32:11 -0500

    fix: add AWS authentication for Kubernetes provider

--- terraform-eks/example/main.tf
+++ terraform-eks/example/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.20.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.20.0"
+    }
   }
 }
 
@@ -14,6 +18,18 @@ provider "aws" {
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      module.eks.cluster_name,
+      "--region",
+      "us-east-1"
+    ]
+  }
 }
 
 # VPC and Subnet configuration
